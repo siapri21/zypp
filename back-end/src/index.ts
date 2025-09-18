0587befd469faed1986852b82766c0a0f2af9c52
siapri21 <siapriouattara21@gmail.com>
2025-09-18 15:18:25 +0200

    deploiement

--- back-end/src/index.ts
+++ back-end/src/index.ts
@@ -8,7 +8,7 @@ import me from "./routes/Me";
 // import authMw from "./middleware/auth";
 
 const app = express();
-app.use(cors({ origin: ["http://localhost:5173", "http://localhost:3000"], credentials: true }));
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
@@ -19,5 +19,5 @@ app.use("/api/me",  me);
 app.get("/api/health", (_req, res) => res.json({ ok: true }));
 
 connectDB(process.env.MONGODB_URI!)
-  .then(() => app.listen(process.env.PORT || 3000, () => console.log("API ready")))
+  .then(() => app.listen(process.env.PORT || 4000, () => console.log("API ready")))
   .catch((e) => { console.error(e); process.exit(1); });
